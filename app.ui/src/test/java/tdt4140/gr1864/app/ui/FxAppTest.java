9881f31c468741060dedd183d9ff22f02586c2e0
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-01-30 09:41:09 +0100

    added examaple JUnit test

--- app.ui/src/test/java/tdt4140/gr1864/app/ui/FxAppTest.java
+++ app.ui/src/test/java/tdt4140/gr1864/app/ui/FxAppTest.java
@@ -10,15 +10,8 @@ import javafx.stage.Stage;
 
 public class FxAppTest extends ApplicationTest {
 
-	@Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
     @Test
     public void testFxApp() {
+    	System.out.println(' ');
     }
 }
