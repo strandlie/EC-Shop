2ac1a7532dc0f7e57b93153bf4e5d2f36ae4dd38
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-01-30 09:58:05 +0100

    removed imports

--- app.ui/src/test/java/tdt4140/gr1864/app/ui/FxAppTest.java
+++ app.ui/src/test/java/tdt4140/gr1864/app/ui/FxAppTest.java
@@ -3,11 +3,6 @@ package tdt4140.gr1864.app.ui;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 public class FxAppTest extends ApplicationTest {
 
     @Test
