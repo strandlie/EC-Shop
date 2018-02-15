4844d565904e66a48c909a9dc4a5601b35f919f4
Torjus Iveland <torjusti@gmail.com>
2018-02-16 00:41:52 +0100

    Remove unused import

--- tdt4140-gr1864/app.core/src/main/resources/tdt4140/gr1864/app/core/DataMocker.java
+++ tdt4140-gr1864/app.core/src/main/resources/tdt4140/gr1864/app/core/DataMocker.java
@@ -1,7 +1,6 @@
 package tdt4140.gr1864.app.core;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
