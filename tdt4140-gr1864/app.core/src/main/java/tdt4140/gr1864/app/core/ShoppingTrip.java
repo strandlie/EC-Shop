9a70e1ccad4de3dca9dd3fea8ebd95f6f9fb37f4
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-02-19 16:04:13 +0100

    A generic constructor for Customer-object testing to use. This should be used for testing Customer only! #4

--- tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/ShoppingTrip.java
+++ tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/ShoppingTrip.java
@@ -16,11 +16,18 @@ public class ShoppingTrip {
 	private int stop;
 	private List<Double> trip;
 	
-	public ShoppingTrip() {
+	public ShoppingTrip(int id, int start, int stop) {
 	/*
 	 * This needs to take in JSON-data from the data-generator and transform
 	 * into into this object-structure.. 
+	 * 
+	 * It is currently ONLY supposed to used for testing Customer-object. IDs
+	 * are not guaranteed to be unique - don't expect this to actually work, it
+	 * is just a template for Customer-object to work.
 	 */
+		this.id = id;
+		this.start = start;
+		this.stop = stop;
 	}
 	
 	public int getId() {
