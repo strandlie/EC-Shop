4e662692cef57d9e817ba0334159b7e215991f2d
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-02-21 13:21:09 +0100

    Restructuring ShoppingTrip to work with new ER-diagram #9

--- tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/ShoppingTrip.java
+++ tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/ShoppingTrip.java
@@ -14,16 +14,13 @@ public class ShoppingTrip {
 	private int id;
 	private int start;
 	private int stop;
-	private List<Double> trip;
+	private List<Coordinate> coordinates;
+	private List<Action> actions;
 	
 	public ShoppingTrip(int id, int start, int stop) {
 	/*
 	 * This needs to take in JSON-data from the data-generator and transform
 	 * into into this object-structure.. 
-	 * 
-	 * It is currently ONLY supposed to used for testing Customer-object. IDs
-	 * are not guaranteed to be unique - don't expect this to actually work, it
-	 * is just a template for Customer-object to work.
 	 */
 		this.id = id;
 		this.start = start;
