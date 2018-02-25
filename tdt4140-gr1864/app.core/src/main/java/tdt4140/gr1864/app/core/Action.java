ac09e65bd587473bcd78849c27994b74eed21b48
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-02-25 16:39:59 +0100

    adds basic Coordinate and Action-objects for #9

--- tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/Action.java
+++ tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/Action.java
@@ -1,5 +1,27 @@
 package tdt4140.gr1864.app.core;
 
 public class Action {
+	
+	private long timeStamp;
+	private long actionType;
+	private long productID;
+	
+	public Action(long timeStamp, long actionType, long productID) {
+		this.timeStamp = timeStamp;
+		this.actionType = actionType;
+		this.productID = productID;
+	}
+	
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	
+	public long getActionType() {
+		return actionType;
+	}
+	
+	public long getProductID() {
+		return productID;
+	}
 
 }
--- tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/Coordinate.java
+++ tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/Coordinate.java
@@ -1,5 +1,27 @@
 package tdt4140.gr1864.app.core;
 
 public class Coordinate {
+	
+	private double x;
+	private double y;
+	private long timeStamp;
+	
+	public Coordinate(double x, double y, long timeStamp) {
+		this.x = x;
+		this.y = y;
+		this.timeStamp = timeStamp;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public long getTimeStamp() {
+		return timeStamp;
+	}
 
 }
