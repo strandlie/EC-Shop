9862f55270376bea066f8c948cd4f3b74984b79b
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-02-25 16:40:42 +0100

    Expands ShoppingTrip with documentation and proper constructor for #9

--- tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/ShoppingTrip.java
+++ tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/ShoppingTrip.java
@@ -2,40 +2,61 @@ package tdt4140.gr1864.app.core;
 
 import java.util.List;
 
-/*
- * TODO:
- * 		- Finalize structure
- * 		- Import and transform JSON-data
- * 		- Implement DatabaseCRUD-framework
- */
-
 public class ShoppingTrip {
-
-	private int id;
+	
+	/* start-time of ShoppingTrip in UNIX-time */
 	private int start;
-	private int stop;
+	/* end-time of ShoppingTrip in UNIX-time */
+	private int end;
+	
+	/* Coordinates that makes up the trip */
 	private List<Coordinate> coordinates;
+	/* Actions perfored during trip */
 	private List<Action> actions;
+
+	public ShoppingTrip(List<Coordinate> coordinates, List<Action> actions) {
+		this.coordinates = coordinates;
+		this.actions = actions;
+		
+		long start = findStart(coordinates);
+		long end = findEnd(coordinates);
+	}
 	
-	public ShoppingTrip(int id, int start, int stop) {
 	/*
-	 * This needs to take in JSON-data from the data-generator and transform
-	 * into into this object-structure.. 
+	 * @param coordinates list of coordinates that makes up the trip
+	 * @return time of first data-point in the list of coordinates
 	 */
-		this.id = id;
-		this.start = start;
-		this.stop = stop;
+	private long findStart(List<Coordinate> coordinates) {
+		long min = coordinates.get(0).getTimeStamp();
+
+		for (Coordinate coord : coordinates) {
+			if (coord.getTimeStamp() < min) {
+				min = coord.getTimeStamp();
+			}
+		}
+		return min;
 	}
 	
-	public int getId() {
-		return id;
+	/*
+	 * @param coordinates list of coordinates that makes up the trip
+	 * @return time of last data-point in the list of coordinates
+	 */
+	private long findEnd(List<Coordinate> coordinates) {
+		long max = coordinates.get(0).getTimeStamp();
+		
+		for (Coordinate coord : coordinates) {
+			if (coord.getTimeStamp() > max) {
+				max = coord.getTimeStamp();
+			}
+		}
+		return max;
 	}
-	
+
 	public int getStart() {
 		return start;
 	}
 	
-	public int getStop() {
-		return stop;
+	public int getEnd() {
+		return end;
 	}
 }
