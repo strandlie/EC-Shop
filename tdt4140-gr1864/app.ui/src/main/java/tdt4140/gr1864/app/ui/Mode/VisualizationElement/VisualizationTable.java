b486c69133a4c0c2f8f61b0429c7cc53d9908993
hstrandlie <hstrandlie@gmail.com>
2018-02-26 16:00:31 +0100

    Started work on VisualizationViewController
    Started to create and define the data model for the Visualization View.

--- tdt4140-gr1864/app.ui/src/main/java/tdt4140/gr1864/app/ui/Mode/VisualizationElement/VisualizationTable.java
+++ tdt4140-gr1864/app.ui/src/main/java/tdt4140/gr1864/app/ui/Mode/VisualizationElement/VisualizationTable.java
@@ -1,10 +1,15 @@
 package tdt4140.gr1864.app.ui.Mode.VisualizationElement;
 
 public class VisualizationTable extends VisualizationElement {
+	
 
 	public VisualizationTable(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
+	}
+	
+	public VisualizationTable(String name, boolean isActive) {
+		super(name, isActive);
+		
 	}
 
 }
--- tdt4140-gr1864/app.ui/src/main/java/tdt4140/gr1864/app/ui/globalUIModel/VisualizationViewController.java
+++ tdt4140-gr1864/app.ui/src/main/java/tdt4140/gr1864/app/ui/globalUIModel/VisualizationViewController.java
@@ -1,5 +1,54 @@
 package tdt4140.gr1864.app.ui.globalUIModel;
 
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class VisualizationViewController {
+	
+	/**
+	 * The attribute representing the Tableview inside the VisualizationView 
+	 */
+	// TODO: Object needs to be changed to Product when merged
+	TableView<Object> table;
+	
+	/**
+	 * The list of products. The TableView listens to this, such that when it is changed the TableView is notified
+	 */
+	ObservableList<Object> products;
+	
+	/**
+	 * The constructor of a view controller without any objects in the table
+	 */
+	public VisualizationViewController() {
+		this.table  = new TableView<Object>();
+		this.products = FXCollections.observableArrayList(this.products);
+	}
+	
+	/**
+	 * The constructor of the view controller with products in the table
+	 * @param products An ArrayList of products, that is put into the ObservableList
+	 */
+	public VisualizationViewController(ArrayList<Object> products) {
+		this.table = new TableView<Object>();
+		this.products = FXCollections.observableArrayList(this.products);
+		
+	}
+	
+	public void initialize() {
+		TableColumn<Object, String> productNameCol = new TableColumn<Object, String>("Product Name");
+		productNameCol.setCellValueFactory(new PropertyValueFactory("productName"));
+		// TODO: Check that there is noe yellow line above here after conversion from Object to Product
+		
+		TableColumn<Object, String> quantitySoldCol = new TableColumn<Object, String>("Quantity");
+		quantitySoldCol.setCellValueFactory(new PropertyValueFactory("quantity"));
+		
+		this.table.getColumns().setAll(productNameCol, quantitySoldCol);
+		
+	}
 
 }
