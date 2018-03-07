56263a8d8d23c508106b033525461cf2736ddca0
Rubenjoh <rubenjo@stud.ntnu.no>
2018-03-07 13:49:53 +0100

    Fixed "\" to "/"

--- tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/DataLoader.java
+++ tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/DataLoader.java
@@ -65,7 +65,7 @@ public class DataLoader {
 		//Finds path by getting URL and converting to URI and then to path 
 		try {
 			URI rerelativeURI = getClass().getClassLoader().getResource(".").toURI();
-			relativePath = Paths.get(rerelativeURI).toFile().toString() + "\\";
+			relativePath = Paths.get(rerelativeURI).toFile().toString() + "/";
 			
 		} catch (URISyntaxException e1) {
 			//If fail to convert to URI use URL path instead
@@ -157,7 +157,7 @@ public class DataLoader {
 		//Finds path by getting URL and converting to URI and then to path 
 		try {
 			URI rerelativeURI = getClass().getClassLoader().getResource(".").toURI();
-			relativePath = Paths.get(rerelativeURI).toFile().toString() + "\\";
+			relativePath = Paths.get(rerelativeURI).toFile().toString() + "/";
 			
 		} catch (URISyntaxException e1) {
 			//If fail to convert to URI use URL path instead
