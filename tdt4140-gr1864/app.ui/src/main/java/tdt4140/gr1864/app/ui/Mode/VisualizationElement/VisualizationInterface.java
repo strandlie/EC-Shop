545b1c0432a3f153f7160c82fe387df23aa467e4
Anders <andershopl@hotmail.com>
2018-03-14 11:36:32 +0100

    Added a basic interface for visualization objects

---
+++ tdt4140-gr1864/app.ui/src/main/java/tdt4140/gr1864/app/ui/Mode/VisualizationElement/VisualizationInterface.java
@@ -0,0 +1,22 @@
+package tdt4140.gr1864.app.ui.Mode.VisualizationElement;
+
+/**
+ * 
+ * @author anders
+ * An interface to be implemented by all visualization objects
+ * Should contain all basic methods used by a controller to 
+ * communicate with a visualization object
+ */
+public interface VisualizationInterface {
+	
+	String getName();
+	
+	boolean isActive();
+	
+	void setActive();
+	
+	/*
+	 * TODO: Add more methods
+	 */
+
+}
