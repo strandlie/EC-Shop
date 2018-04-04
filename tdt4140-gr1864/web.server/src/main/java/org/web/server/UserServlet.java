76eb1aa4a309d78f8dafa5777e384b7cd71a9ccf
Torjus Iveland <torjusti@gmail.com>
2018-04-04 12:57:40 +0200

    Add servlets for listing and retrieving receipts

--- tdt4140-gr1864/web.server/src/main/java/org/web/server/UserServlet.java
+++ tdt4140-gr1864/web.server/src/main/java/org/web/server/UserServlet.java
@@ -2,31 +2,68 @@ package org.web.server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.web.server.ReceiptServlet.ReceiptSerializer;
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import tdt4140.gr1864.app.core.Receipt;
+import tdt4140.gr1864.app.core.ShoppingTrip;
 import tdt4140.gr1864.app.core.database.DataLoader;
+import tdt4140.gr1864.app.core.databasecontrollers.ShoppingTripDatabaseController;
 
 public class UserServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	
+	private ShoppingTripDatabaseController shoppingTripController = new ShoppingTripDatabaseController();
+
 	@Override
 	public void init() throws ServletException {
 		super.init();
 	}
 
-	// TODO:
-	//	Handle GETs with query parameters with UserID
-	//	Authentication????
+	private class UserData {
+		public List<Integer> shoppingTrips = new ArrayList<>();
+		
+		public UserData(List<ShoppingTrip> trips) {
+			for (ShoppingTrip trip : trips) {
+				shoppingTrips.add(trip.getShoppingTripID());
+			}
+		}
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+        response.setContentType("application/json");
+        
+		if (!request.getParameterMap().containsKey("id")) {
+	        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	        return;
+		}
+		
+		int id = Integer.valueOf(request.getParameter("id"));
+
+		List<ShoppingTrip> trips = shoppingTripController.getTripsForCustomer(id);
+		
+		if (trips == null) {
+	        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+	        return;
+		}
+		
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Hello UserServlet</h1>");
-        response.getWriter().println("session=" + request.getSession(true).getId());
+		ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String output = writer.writeValueAsString(new UserData(trips));
+		response.getWriter().println(output);
     }
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
