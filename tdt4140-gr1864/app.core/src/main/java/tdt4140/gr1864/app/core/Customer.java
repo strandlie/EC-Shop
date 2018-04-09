fa4e0a4a47ab421a3351272dfd88e4b54f79095b
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-04-09 14:35:42 +0200

    Refactors #53 customerId to customerID

--- tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/Customer.java
+++ tdt4140-gr1864/app.core/src/main/java/tdt4140/gr1864/app/core/Customer.java
@@ -19,7 +19,7 @@ import tdt4140.gr1864.app.core.interfaces.UserInterface;
 public class Customer extends Observable implements Model, UserInterface {
 
 	@JsonProperty("customerID")
-	private int customerId;
+	private int customerID;
 
 	@JsonProperty
 	private String firstName;
@@ -52,7 +52,7 @@ public class Customer extends Observable implements Model, UserInterface {
 	 */
 	public Customer(int customerID, String firstName, String lastName, 
 			List<ShoppingTrip> shoppingTrips) {
-		this.customerId = customerID;
+		this.customerID = customerID;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.shoppingTrips = shoppingTrips;
@@ -71,7 +71,7 @@ public class Customer extends Observable implements Model, UserInterface {
 	 */
 	public Customer(String firstName, String lastName, int customerID,
 			 String address, int zip, boolean anonymous) {
-		this.customerId = customerID;
+		this.customerID = customerID;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
@@ -145,7 +145,7 @@ public class Customer extends Observable implements Model, UserInterface {
 		// Is a primitive and does not need a check
 		this.anonymous = anonymous;
 
-		this.customerId = customerID;
+		this.customerID = customerID;
 	}
 
 	/**
@@ -156,7 +156,7 @@ public class Customer extends Observable implements Model, UserInterface {
 	public Customer(String firstName, String lastName, int customerID) { 
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.customerId = customerID;
+		this.customerID = customerID;
 		this.anonymous = false;
 	}
 	
@@ -171,15 +171,15 @@ public class Customer extends Observable implements Model, UserInterface {
 
 	@JsonIgnore
 	public int getID() {
-		return customerId;
+		return customerID;
 	}
 	
 	public int getCustomerID() {
-		return customerId;
+		return customerID;
 	}
 
 	public void setCustomerId(int userId) {
-		this.customerId = userId;
+		this.customerID = userId;
 	}
 
 	public String getFirstName() {
@@ -235,11 +235,11 @@ public class Customer extends Observable implements Model, UserInterface {
 		List<ShoppingTrip> allTrips = stdc.retrieveAllShoppingTrips();
 		
 		// List of all shopping trips for this customer	
-		List<ShoppingTrip> customerTrips = stdc.retrieveAllShoppingTripsForCustomer(this.customerId);
+		List<ShoppingTrip> customerTrips = stdc.retrieveAllShoppingTripsForCustomer(this.customerID);
 
 		// PRINT
 		System.out.println(allTrips.size() + " all trips size");
-		System.out.println(customerTrips.size() + " customer all trips size, customerID: " + this.customerId);
+		System.out.println(customerTrips.size() + " customer all trips size, customerID: " + this.customerID);
 		
 		
 		
@@ -445,7 +445,7 @@ public class Customer extends Observable implements Model, UserInterface {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		Customer customer = (Customer) o;
-		return customerId == customer.customerId &&
+		return customerID == customer.customerID &&
 				zip == customer.zip &&
 				age == customer.age &&
 				numberOfPersonsInHousehold == customer.numberOfPersonsInHousehold &&
@@ -460,7 +460,7 @@ public class Customer extends Observable implements Model, UserInterface {
 	@Override
 	public int hashCode() {
 
-		return Objects.hash(customerId, firstName, lastName, address, zip, gender, age, numberOfPersonsInHousehold, shoppingTrips, recommendedProductID);
+		return Objects.hash(customerID, firstName, lastName, address, zip, gender, age, numberOfPersonsInHousehold, shoppingTrips, recommendedProductID);
 	}
 
 	public boolean getAnonymous() {
